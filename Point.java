69c52f1dca1a7403967ca9569c8730209006dbee
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-11-01 23:03:25 -0500

    yay

--- Point.java
+++ Point.java
@@ -20,6 +20,13 @@ public class Point {
         return y;
     }
 
-    
+
+    public double distanceTo(Point other){
+        return (Math.sqrt(Math.pow((this.y - other.y),2) + Math.pow((this.x - other.x),2)));
+    }
+
+    public boolean equals(Point other){
+        return (this.x == other.x && this.y == other.y);
+    }
 
 }
